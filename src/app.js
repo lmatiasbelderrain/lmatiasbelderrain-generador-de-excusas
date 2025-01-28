03e8638aeda3de25b6f9ef9835be424b0938413f
lmatiasbelderrain <Matibelde@hotmail.com>
2025-01-28 21:37:03 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,73 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let first = "A ";
+  let adj = [
+    "two headed ",
+    "nuclear ",
+    "angry ",
+    "lonely ",
+    "crazy ",
+    "glowing ",
+    "smelly ",
+    "slow ",
+    "old "
+  ];
+  let who = [
+    "jogger ",
+    "racoon ",
+    "dog ",
+    "merchant ",
+    "driver ",
+    "comedian ",
+    "pinecone "
+  ];
+  let action = [
+    "took my ",
+    "threw my ",
+    "kicked my ",
+    "stole my ",
+    "burned my ",
+    "bit my ",
+    "hit my "
+  ];
+  let what = [
+    "toe ",
+    "car ",
+    "watch ",
+    "shoe ",
+    "wallet ",
+    "shirt ",
+    "keys ",
+    "computer ",
+    "phone ",
+    "sandwich "
+  ];
+  let where = [
+    "on the street",
+    "in my house",
+    "in my driveway",
+    "in front of the office",
+    "near the mall",
+    "near the toilet",
+    "at the bus station"
+  ];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+
+  let r1 = Math.floor(Math.random() * adj.length);
+  let r2 = Math.floor(Math.random() * who.length);
+  let r3 = Math.floor(Math.random() * action.length);
+  let r4 = Math.floor(Math.random() * what.length);
+  let r5 = Math.floor(Math.random() * where.length);
+  let r6 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    first + adj[r1] + who[r2] + action[r3] + what[r4] + where[r5] + when[r6];
 };
